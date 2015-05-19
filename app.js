a4b273717dfe139cbe1f5ade72d896f2d68ba05a
Russell DuBrock <russell.dubrock@gmail.com>
2015-05-19 13:15:12 -0700

    upload working, saving blog posts

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
@@ -9,6 +10,7 @@ var bcrypt = require('bcrypt');
 var https = require('https');
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
+var multipart = require('connect-multiparty');
 // var mailgunLogin = require('./mailgunlogin.js');
 // var transporter = nodemailer.createTransport(smtpTransport({
 //   host: 'smtp.mailgun.org',
@@ -21,12 +23,20 @@ var smtpTransport = require('nodemailer-smtp-transport');
 var jwtKey = 'S2F7JF6ltWX4cgC5VDcQ';
 var password = 'dirtshouldcakehowever';
 var userName = 'Franchfry';
+var blogTitle = '';
+var blogBody = '';
+var blogImg = '';
 
 // view engine setup
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'));
+app.use(multipart({
+    uploadDir: 'public/images/blogpost'
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -41,18 +51,73 @@ app.get('/login', function(req, res) {
 app.post('/login', function(req, res) {
  
   if (req.body.user === userName && req.body.password === password) {
-      res.cookie('token', authenticate(userName));
+      res.cookie('token', authenticate(userName, password));
       res.redirect('/');
   } else {
       res.end('invalid username/password combo');
   }
 });
 
-function authenticate (userId){
+app.post('/authenticate', [jwtAuth], function(req, res){
+  if(req.userStatus === 'loggedIn'){
+    res.status(200).end();
+  } else {
+    res.status(500).end();
+  }
+})
+
+exports.create = function (req, res, next) {
+    var data = _.pick(req.body, 'type')
+        , uploadPath = path.normalize(cfg.data + '/uploads')
+        , file = req.files.file;
+
+        console.log(file.name); //original name (ie: sunset.png)
+        console.log(file.path); //tmp path (ie: /tmp/12345-xyaz.png)
+    console.log(uploadPath); //uploads directory: (ie: /home/user/data/uploads)
+};
+
+app.post('/uploads', [jwtAuth], function(req, res){
+  fs.readdir('public/images/blogpost', function(err, files){
+    if(err) throw err;
+    var fileString = files.toString();
+    res.end(fileString);
+  })  
+});
+
+app.post('/blogsave', [jwtAuth], function(req, res){
+  if (req.userStatus === 'loggedIn') {
+    console.log(req.body);
+    blogTitle = req.body.post-title;
+  }
+})
+
+function jwtAuth (req, res, next){
+  var token = (req.cookies.token)
+  if (token) {  
+    try {
+      var decoded = jwt.decode(token, jwtKey); //check for decoded.email
+      if (decoded.exp <= Date.now()){
+        res.end('Access token expired', 400);
+      }
+      if (decoded.iss === "Franchfry" && decoded.pass === password) {
+        req.userStatus = 'loggedIn';
+        next();
+      }
+    } catch (err) {
+      return next();
+    }
+  } else {
+    next();
+  }
+
+};
+
+function authenticate (userId, pass){
   var expires = moment().add(7, 'days').valueOf();
   var token = jwt.encode({
     iss: userId,
     exp: expires,
+    pass: pass 
   }, jwtKey);
   return token;
 }
---
+++ public/javascripts/app.js
@@ -0,0 +1,80 @@
+var franSite = angular.module('franSite', ['ngFileUpload'])
+
+.controller('UserController', ['UserService', '$http', function(UserService, $http){
+  var self = this;
+
+  this.imageDelete = function(){
+     // UserService.images.splice(0, 100);
+     console.log(this.images);
+  }
+
+  this.editor = true;
+
+  this.toggleEditor = function(){
+    this.editor = !this.editor;
+  }
+
+  this.images = UserService.images;
+
+  this.loggedIn = false;
+  
+  if(document.cookie){
+    $http.post('/authenticate').
+    success(function(data, status, headers, config) {
+      console.log(status);
+      self.loggedIn = true;
+      $http.post('/uploads').
+        success(function(data, status, headers, config) {
+          var images = data.split(",");
+            images.shift();
+            for (var i = 0; i < images.length; i++) {
+              UserService.images.push(images[i]);
+            };
+        }).
+        error(function(data, status, headers, config) {
+          console.log(status);
+        });
+
+    }).
+    error(function(data, status, headers, config) {
+      console.log(data);
+    });
+   
+  }
+}])
+
+.controller('UploadController', ['$scope', 'Upload', 'UserService', function ($scope, Upload, UserService) {
+  
+  $scope.$watch('files', function () {
+      $scope.upload($scope.files);
+  });
+  $scope.log = '';
+
+  $scope.upload = function (files) {
+      if (files && files.length) {
+          for (var i = 0; i < files.length; i++) {
+              var file = files[i];
+              Upload.upload({
+                  url: '/uploads',
+                  file: file
+              }).progress(function (evt) {
+                  var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                  $scope.log = 'progress: ' + progressPercentage + '% ' +
+                              evt.config.file.name + '\n' + $scope.log;
+              }).success(function (data, status, headers, config) {
+                  var images = data.split(",");
+                  images.shift();
+                  for (var i = 0; i < images.length; i++) {
+                    UserService.images.push(images[i]);
+                  };
+                  console.log(UserService.images);
+              });
+          }
+      }
+  };
+}])
+
+.service('UserService', function() {
+  this.images = [];
+});
+
